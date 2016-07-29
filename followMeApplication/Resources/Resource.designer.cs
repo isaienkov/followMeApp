6a598f5dc21fb44ee0f4eba125c48225c33fe5b6
kostya <is_kos@ukr.net>
2016-07-30 00:34:19 +0300

    Design changes

--- followMeApplication/Resources/Resource.designer.cs
+++ followMeApplication/Resources/Resource.designer.cs
@@ -44,8 +44,17 @@ namespace followMeApplication
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050003
+			public const int butSignIn = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int butSignUp = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int txt1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int txt2 = 2131034114;
 			
 			static Id()
 			{
--- followMeApplication/obj/Debug/android/isaienkov/followmeapplication/R.java
+++ followMeApplication/obj/Debug/android/isaienkov/followmeapplication/R.java
@@ -11,7 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class id {
-        public static final int myButton=0x7f050000;
+        public static final int butSignIn=0x7f050003;
+        public static final int butSignUp=0x7f050001;
+        public static final int txt1=0x7f050000;
+        public static final int txt2=0x7f050002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
